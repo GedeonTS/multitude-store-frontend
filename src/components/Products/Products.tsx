046e0c910d825bf1ae3320d42b650c8d2e20b09d
GedeonTS <gedeontshobohwa1@gmail.com>
2024-01-19 00:32:29 +0200

    Add products styles

--- src/components/Products/Products.tsx
+++ src/components/Products/Products.tsx
@@ -1,4 +1,20 @@
 import './Product.scss';
+import StarRating from "../FiveStars/StarRating";
+import styled from "styled-components";
+
+const Stars = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+min-height: 50px;
+input[type="radio"] {
+  display: none;
+}
+.star {
+  cursor: pointer;
+  transition: color 200ms;
+}
+`;
 
 function Products() {
     const ProductsData: {
@@ -15,70 +31,70 @@ function Products() {
         {
             id: 1,
             name: 'Product 1',
-            description: 'Product 1 description',
+            description: 'This is an american product, manufactured in LA',
             price: 100,
             image: 'https://websitedemos.net/electronic-store-04/wp-content/uploads/sites/1055/2022/03/electronic-store-category-image-06.jpg',
         },
         {
             id: 2,
             name: 'Product 2',
-            description: 'Product 2 description',
+            description: 'This is an american product, manufactured in LA',
             price: 200,
             image: 'https://websitedemos.net/electronic-store-04/wp-content/uploads/sites/1055/2022/03/electronic-store-category-image-06.jpg',
         },
         {
             id: 3,
             name: 'Product 3',
-            description: 'Product 3 description',
+            description: 'This is an american product, manufactured in LA',
             price: 300,
             image: 'https://websitedemos.net/electronic-store-04/wp-content/uploads/sites/1055/2022/03/electronic-store-category-image-06.jpg',
         },
         {
             id: 4,
             name: 'Product 4',
-            description: 'Product 4 description',
+            description: 'This is an american product, manufactured in LA',
             price: 400,
             image: 'https://websitedemos.net/electronic-store-04/wp-content/uploads/sites/1055/2022/03/electronic-store-category-image-06.jpg',
         },
         {
             id: 5,
             name: 'Product 5',
-            description: 'Product 5 description',
+            description: 'This is an american product, manufactured in LA',
             price: 500,
             image: 'https://websitedemos.net/electronic-store-04/wp-content/uploads/sites/1055/2022/03/electronic-store-category-image-06.jpg',
         },
         {
             id: 6,
             name: 'Product 6',
-            description: 'Product 6 description',
+            description: 'This is an american product, manufactured in LA',
             price: 600,
             image: 'https://websitedemos.net/electronic-store-04/wp-content/uploads/sites/1055/2022/03/electronic-store-category-image-06.jpg',
         },
         {
             id: 7,
             name: 'Product 7',
-            description: 'Product 7 description',
+            description: 'This is an american product, manufactured in LA',
             price: 700,
             image: 'https://websitedemos.net/electronic-store-04/wp-content/uploads/sites/1055/2022/03/electronic-store-category-image-06.jpg',
         },
         {
             id: 8,
             name: 'Product 8',
-            description: 'Product 8 description',
+            description: 'This is an american product, manufactured in LA',
             price: 800,
             image: 'https://websitedemos.net/electronic-store-04/wp-content/uploads/sites/1055/2022/03/electronic-store-category-image-06.jpg',
         },
         {
             id: 9,
             name: 'Product 9',
-            description: 'Product 9 description',
+            description: 'This is an american product, manufactured in LA',
             price: 900,
             image: 'https://websitedemos.net/electronic-store-04/wp-content/uploads/sites/1055/2022/03/electronic-store-category-image-06.jpg',
         },
         {
             id: 10,
             name: 'Product 10',
-            description: 'Product 10 description',
+            description: 'This is an american product, manufactured in LA',
             price: 1000,
             image: 'https://websitedemos.net/electronic-store-04/wp-content/uploads/sites/1055/2022/03/electronic-store-category-image-06.jpg',
         }]}
@@ -91,10 +107,13 @@ function Products() {
                     <div className='product-item-inner'>
                         <img src={product.image} alt={product.name} />
                         <div className='product-item-info'>
-                            <h3>{product.name}</h3>
-                            <p>{product.description}</p>
-                            <p>{product.price}</p>
-                            <button>Add to cart</button>
+                         
+                            <Stars className='stars'><StarRating/></Stars>
+                        
+                            <div>
+                            <p className='description'>{product.description}</p>
+                            <p className='price'>${product.price}</p>
+                            </div>
                         </div>
                     </div>
                 </div>
