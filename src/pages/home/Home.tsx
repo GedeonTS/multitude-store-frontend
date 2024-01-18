d4c6ede7f8bcd60f97740d956e1c8c4488697dbe
GedeonTS <gedeontshobohwa1@gmail.com>
2024-01-19 01:05:57 +0200

    Add sample promotions

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,10 +1,14 @@
 import './Home.scss';
 import Headline from '../../components/Headline/Headline';
-
+import Products from '../../components/Products/Products';
+import Promotion from '../../components/Promotion/Promotion';
 function Home() {
   return (
     <section className='home-section'>
      <Headline/>
+     <Products/>
+     <Promotion/>
+     <Products/>
     </section>
   )
 }
