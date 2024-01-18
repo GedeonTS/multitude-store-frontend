556146cb08b60355cb1326e893af653aeea3da37
GedeonTS <gedeontshobohwa1@gmail.com>
2024-01-19 01:05:37 +0200

    Create promotion component

---
+++ src/components/Promotion/Promotion.tsx
@@ -0,0 +1,40 @@
+import './Promotion.scss';
+import imgUrl1 from '../../assets/case.jpg';
+import imgUrl2 from '../../assets/phones.jpg';
+
+function Promotion() {
+    const divStyle1 = {
+        color: 'white',
+        fontSize: '20px',
+        textAlign: 'center',
+        backgroundImage: 'url(' + imgUrl1 + ')',
+        backgroundSize: 'cover',
+      };
+
+      const divStyle2 = {
+        color: 'white',
+        fontSize: '20px',
+        textAlign: 'center',
+        backgroundImage: 'url(' + imgUrl2 + ')',
+        backgroundSize: 'cover',
+      };
+  return (
+    <section className="promotion-section">
+        <div className="promotion-section-inner" style={divStyle1}>
+            <h2>The only Case you need.</h2>
+            <a href="#" className="btn">
+          Shop Now
+        </a>
+        </div>
+        <div className="promotion-section-inner" style={divStyle2}>
+        <p>BRAND DAY</p>
+        <h2>Get 30% Off</h2>
+        <a href="#" className="btn">
+          Shop Now
+        </a>
+        </div>
+    </section>
+  )
+}
+
+export default Promotion
