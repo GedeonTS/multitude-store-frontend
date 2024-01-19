da5e3f7c12a5b687636c566bbb490e31f342ea6f
GedeonTS <gedeontshobohwa1@gmail.com>
2024-01-19 17:52:30 +0200

    Import newly created components

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,16 +1,23 @@
-import './Home.scss';
-import Headline from '../../components/Headline/Headline';
-import Products from '../../components/Products/Products';
-import Promotion from '../../components/Promotion/Promotion';
+import "./Home.scss";
+import Headline from "../../components/Headline/Headline";
+import Products from "../../components/Products/Products";
+import Promotion from "../../components/Promotion/Promotion";
+import TopBrands from "../../components/TopBrands/TopBrands";
+import Comments from "../../components/Comments/Comments";  
+
 function Home() {
   return (
-    <section className='home-section'>
-     <Headline/>
-     <Products/>
-     <Promotion/>
-     <Products/>
+    <section className="home-section">
+      <Headline />
+      <Products category={'Top sales'}/>
+      <Promotion />
+      <Products category={'Audio & Video'}/>
+      <Promotion />
+      <Products category={'Home appliances'}/>
+      <TopBrands />
+      <Comments />
     </section>
-  )
+  );
 }
 
-export default Home
+export default Home;
