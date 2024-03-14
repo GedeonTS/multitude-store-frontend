bb64a8bef982b91773a8c776b6a851703010b90d
GedeonTS <gedeontshobohwa1@gmail.com>
2024-03-14 22:33:05 +0200

    Refactor the upload section

--- src/components/Upload/UploadFile.tsx
+++ src/components/Upload/UploadFile.tsx
@@ -1,27 +1,40 @@
-import axios from "axios";
-function UploadFile() {
-  const preset_key = "";
-  const cloud_name = "";
-  const [image, setImage] = useState();
+import React, { useState } from 'react';
+import "./UploadFile.scss"
 
-  function handleFile(event) {
-    const file = event.target.files[0];
-    const formData = new FormData();
-    formData.append("file", file);
-    formData.append("upload_preset", preset_key);
-    axios
-      .post("https://api.cloudinary.com/v1_1/${}/image/upload", formData)
-      .then((res) => console.log(res))
-      .catch((err) => console.log(err));
-  }
+interface UploadFileProps {
+  // Add any additional props you might need here
+}
+
+const UploadFile: React.FC<UploadFileProps> = () => {
+
+  const [selectedImage, setSelectedImage] = useState<string | null>(null);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        setSelectedImage(e.target?.result as string);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
 
   return (
-    <div>
-      <div className="">
-        <input type="file" onChange={handleFile} />
-      </div>
+    <div className="circle-image-upload">
+      {!selectedImage ?<p>Add</p>:""}
+      <input
+        type="file"
+        accept="image/*"
+        onChange={handleChange}
+      />
+
+      {selectedImage && (
+        <img src={selectedImage} alt="Uploaded image" />
+      )}
     </div>
   );
-}
+};
+
 
 export default UploadFile;
