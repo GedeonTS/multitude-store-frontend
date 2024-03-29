f8d6cc359435033b1845c5841f9970a111bc3935
GedeonTS <gedeontshobohwa1@gmail.com>
2024-03-29 22:03:58 +0200

    Add link to the login page

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar/Navbar';
 import './App.scss';
 import Footer from './components/Footer/Footer';
 import Description from './pages/description/Description';
+import Login from './pages/login/Login';
 import Category from './pages/Category/Category';
 import Cart from './pages/Cart/Cart';
 import AddItemForm from './components/NewItem/AddItemForm';
@@ -21,6 +22,7 @@ function App() {
         <Route path="/:itemId" element={<Description />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/add-item" element={<AddItemForm />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
       <Footer />
     </div>
