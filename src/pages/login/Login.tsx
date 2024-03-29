b4eecd0df7aed9103c64427881013a097a7ca01c
GedeonTS <gedeontshobohwa1@gmail.com>
2024-03-29 22:03:44 +0200

    Adjust login page styles and add connect with google option

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-
+import { useState } from 'react'
+import './Login.scss'
 const Login = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
@@ -11,6 +11,9 @@ const Login = () => {
   
     return (
       <div className="login-container">
+      <div className="login-container-inner1">
+      <div className="login-container-inner2">
+      <div className="login-container-inner3">
         <h2>Login</h2>
         <form className="login-form">
           <label htmlFor="username">Username:</label>
@@ -32,7 +35,12 @@ const Login = () => {
           <button type="button" onClick={handleLogin}>
             Login
           </button>
+
+          <a className="google-connect" href='/google'>Connect with Google</a>
         </form>
+      </div>
+      </div>
+      </div>
       </div>
     );
 }
